0fc6ae8652a137a5abbea894fbd9228b1e266115
Preeth <preeth.mcu@gmail.com>
2023-02-19 13:29:59 +0530

    mail send option

--- info/forms/contact.php
+++ info/forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'janagan.gu1998@gmail.com';
+  /* $receiving_email_address = 'janagan.gu1998@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -37,5 +37,42 @@
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
-?>
+  echo $contact->send(); 
+?> */
+
+if (isset($_POST['submit'])) {
+    $fromEmail = $_POST['email'];
+    $toEmail = "janagan.gu1998@gmail.com";
+    $subjectName = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = $toEmail;
+    $subject = $subjectName;
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: '.$fromEmail.'<'.$fromEmail.'>' . "\r\n".'Reply-To: '.$fromEmail."\r\n" . 'X-Mailer: PHP/' . phpversion();
+    $message = '<!doctype html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport"
+					  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+				<title>Document</title>
+			</head>
+			<body>
+			<span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">'.$message.'</span>
+				<div class="container">
+                 '.$message.'<br/>
+                    Regards<br/>
+                  '.$fromEmail.'
+				</div>
+			</body>
+			</html>';
+    $result = @mail($to, $subject, $message, $headers);
+
+    echo '<script>alert("Email sent successfully !")</script>';
+    echo '<script>window.location.href="index.php";</script>';
+}
+
+?> 
