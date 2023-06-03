cd3e4fd98843dbca3d16652cf7895aaa6e644270
Janagan Gunasekaran <praeclarus.me@gmail.com>
2023-06-03 23:32:45 +0530

    completion of portfolio commit

--- pbr/assets/js/steller.js
+++ pbr/assets/js/steller.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 
 			$('html, body').animate({
 				scrollTop: $(hash).offset().top
-			}, 700, function(){
+			}, 300, function(){
 				window.location.hash = hash;
 			});
       	} 
